9d18bf80bf9a94bd5b028844e66b5f3b690a30ad
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-03-13 11:59:21 +0800

    wip: components

---
+++ 008/main.js
@@ -0,0 +1,21 @@
+customElements.define("my-h2",
+    class extends HTMLElement {
+        constructor() {
+            super()
+            const tpl = document.getElementById("myh2")
+            const shadowRoot = this.attachShadow({ mode: "open" })
+            shadowRoot.append(tpl.content.cloneNode(true))
+        }
+    }
+)
+
+customElements.define("my-btn",
+    class extends HTMLElement {
+        constructor() {
+            super()
+            const tpl = document.getElementById("mybtn")
+            const shadowRoot = this.attachShadow({ mode: "open" })
+            shadowRoot.append(tpl.content.cloneNode(true))
+        }
+    }
+)
