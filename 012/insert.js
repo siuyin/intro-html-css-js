0a37bc4698713a5dec23e20a1b4a706e94979240
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-03-16 12:39:29 +0800

    feat: add web components with modules

--- 012/insert.js
+++ 012/insert.js
@@ -1,6 +1,7 @@
 export async function insTemplate(filename, id) {
     const f = await fetch(filename)
     const t = await f.text()
+
     const host = document.getElementById(id)
     const d = document.createElement("div")
     d.innerHTML = t
