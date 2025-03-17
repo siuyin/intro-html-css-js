62011c6293db14bb4f19f4d90ed3ef4305c01942
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-03-17 14:57:27 +0800

    feat: add delay.js

--- 013/main.js
+++ 013/main.js
@@ -21,10 +21,46 @@ main()
 
 
 function mdHandler(req) {
+
+    const txt = `# This is markdown on \`Deno\`
+The quick brown fox jumps over the lazy dog.  
+The rain in spain falls mainly on the plain.  
+A stitch in time saves nine.  
+The cat sat on the mat.
+
+Next we have an ordered list:
+1. a
+1. b
+1. c
+
+## Next header
+And a bullet list:
+* apples
+* bananas
+* cranberries
+`
     const hdrs = new Headers()
     hdrs.set("Transfer-Encoding", "chunked")
     hdrs.set("Access-Control-Allow-Origin", "*")
 
-    const res = new Response("markdown here", {headers:hdrs})
+    const res = new Response(mdStream(txt), { headers: hdrs })
     return res
+}
+
+import { delay } from "./delay.js"
+function mdStream(txt) {
+    const p = txt.split(" ")
+    const en = new TextEncoder()
+    let n = 0
+
+    return new ReadableStream({
+        async pull(ctrl) {
+            const wd = p[n] + " "
+            if (n == p.length) { ctrl.close(); return }
+
+            ctrl.enqueue(en.encode(wd))
+            await delay(150)
+            n++
+        }
+    })
 }
